7c4d8225a23208beb266c3f129fcbbc1b8256ae8
lezintirabassi <tirabassileandro30@gmail.com>
2024-05-28 13:18:08 -0300

    alteracoes

--- ap2.py
+++ ap2.py
@@ -1,72 +1,66 @@
 import requests
-from flask import Flask
+import time
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask
 
-# Configuração do Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///enderecos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicialização do SQLAlchemy e Flask-Migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Modelo Endereco
-class Endereco(db.Model):
-    __tablename__ = 'Enderecos'
-    cep = db.Column(db.String, primary_key=True)
-    logradouro = db.Column(db.String)
-    complemento = db.Column(db.String)
-    bairro = db.Column(db.String)
-    localidade = db.Column(db.String)
-    uf = db.Column(db.String)
-    ibge = db.Column(db.String)
-    gia = db.Column(db.String)
-    ddd = db.Column(db.String)
-    siafi = db.Column(db.String)
+class Enderecos(db.Model):
+    cep = db.Column(db.String(8), primary_key=True)
+    logradouro = db.Column(db.String(100))
+    complemento = db.Column(db.String(100))
+    bairro = db.Column(db.String(100))
+    localidade = db.Column(db.String(100))
+    uf = db.Column(db.String(2))
+    ibge = db.Column(db.String(20))
+    gia = db.Column(db.String(10))
+    ddd = db.Column(db.String(10))
+    siafi = db.Column(db.String(10))
 
-# Função para consultar e inserir endereço
-def consultar_e_inserir_endereco(cep):
+def pesquisar_e_salvar_endereco(cep):
     url = f'https://viacep.com.br/ws/{cep}/json/'
-    resposta = requests.get(url)
-    if resposta.status_code == 200:
-        data = resposta.json()
+    try:
+        resposta = requests.get(url)
+        resposta.raise_for_status()
+        dados_endereco = resposta.json()
+        print(dados_endereco)
         
-        # Verifica se o CEP já existe no banco de dados
-        endereco_existente = Endereco.query.filter_by(cep=cep).first()
-        if endereco_existente:
-            print(f"O endereço com o CEP {cep} já existe no banco de dados. Pulando para o próximo CEP...")
-            return
-        
-        endereco = Endereco(
-            cep=data['cep'],
-            logradouro=data.get('logradouro', ''),
-            complemento=data.get('complemento', ''),
-            bairro=data.get('bairro', ''),
-            localidade=data.get('localidade', ''),
-            uf=data.get('uf', ''),
-            ibge=data.get('ibge', ''),
-            gia=data.get('gia', ''),
-            ddd=data.get('ddd', ''),
-            siafi=data.get('siafi', '')
-        )
-        db.session.add(endereco)
-        try:
-            db.session.commit()
-            print("Endereço inserido com sucesso!")
-        except Exception as e:
-            db.session.rollback()  # Desfaz a transação para evitar o erro
-            print("Erro ao inserir endereço:", e)
-    else:
-        print("Erro ao consultar o CEP:", resposta.status_code)
+        if 'erro' not in dados_endereco:
+            with app.app_context():
+                enderecos = Enderecos(
+                    cep=dados_endereco.get('cep', 'N/A').replace('-', ''),
+                    logradouro=dados_endereco.get('logradouro', 'N/A'),
+                    complemento=dados_endereco.get('complemento', 'N/A'),
+                    bairro=dados_endereco.get('bairro', 'N/A'),
+                    localidade=dados_endereco.get('localidade', 'N/A'),
+                    uf=dados_endereco.get('uf', 'N/A'),
+                    ibge=dados_endereco.get('ibge', 'N/A'),
+                    gia=dados_endereco.get('gia', 'N/A'),
+                    ddd=dados_endereco.get('ddd', 'N/A'),
+                    siafi=dados_endereco.get('siafi', 'N/A')
+                )
+                db.session.add(enderecos)
+                db.session.commit()
+                print(f"Endereço salvo: {dados_endereco}")
+        else:
+            print('CEP não encontrado')
+    except requests.RequestException as e:
+        print(f"Erro ao consultar a API: {e}")
 
 if __name__ == '__main__':
     with app.app_context():
-        # Inicializa as tabelas no banco de dados (caso ainda não existam)
         db.create_all()
-        consultar_e_inserir_endereco('04013000')
-        consultar_e_inserir_endereco('01001000')
-        consultar_e_inserir_endereco('05607000')
-        consultar_e_inserir_endereco('01310200')
-    app.run(debug=True)
+
+    while True:
+        cep = input("Digite o CEP que deseja pesquisar (ou digite 'sair' para sair): ")
+        if cep.lower() == 'sair':
+            break
+        pesquisar_e_salvar_endereco(cep)
+
+    time.sleep(5)
